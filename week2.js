412b7c8600dc68b32865ca9d5446e51ab2b54464
Vonetta Stevenson <vonettastevenson@gmail.com>
2019-06-08 16:10:28 -0700

    get and set

--- week2.js
+++ week2.js
@@ -1,9 +1,25 @@
 hash = str => {
   let num = 0;
   for (let i = 0; i < str.length; i++) {
-    num += str.charCodeAt(str[i]) % 2;
+    num += str.charCodeAt(str[i]);
   }
-  return num;
+  return num % 1000;
 };
 
 console.log(hash("testimony"));
+
+function createArr() {
+  this.arr = [];
+
+  this.set = (i, val) => {
+    let currentHash = hash(i);
+    return (this.arr[currentHash] = val);
+  };
+
+  this.get = i => {
+    let currentHash = hash(i);
+    return this.arr[currentHash];
+  };
+}
+
+let myArr = new createArr();
